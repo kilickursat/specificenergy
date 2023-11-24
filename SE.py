9e37e3736029daaaa2b35a10464a134b55701aa4
Kursat Kilic <kilic_kursat@hotmail.com>
2023-11-24 15:01:46 +0900

    Update SE.py

--- SE.py
+++ SE.py
@@ -56,33 +56,6 @@ if page == 'Online':
 
 elif page == 'Visualization':
     st.title('Visualization of Model Performance')
-
-        # Create a dictionary mapping variable names to strings
-    params = {
-        'Pressure gauge 1 (kPa)': (0, 1000, 500),
-        'Pressure gauge 2 (kPa)': (0, 1000, 500),
-        'Pressure gauge 3 (kPa)': (0, 1000, 500),
-        'Pressure gauge 4 (kPa)': (0, 1000, 500),
-        'Digging velocity left (mm/min)': (0, 1000, 500),
-        'Digging velocity right (mm/min)': (0, 1000, 500),
-        'advancement speed ': (0, 100, 50),
-        'Shield jack stroke left (mm)': (0, 1000, 500),
-        'Shield jack stroke rigth (mm)': (0, 1000, 500),
-        'Propulsion pressure (MPa)': (0, 10, 5),
-        'Total thrust (kN)': (0, 1000, 500),
-        'Cutter torque (kN.m)': (0, 1000, 500),
-        'Cutterhead rotation speed (rpm)': (0, 1000, 500),
-        'Screw pressure (MPa)': (0, 10, 5),
-        'Screw rotation speed (rpm)': (0, 100, 50),
-        'gate opening (%)': (0, 100, 50),
-        'Mud injection pressure (MPa)': (0, 10, 5),
-        'Add mud flow (L/min)': (0, 100, 50),
-        'Back in injection rate (%)': (0, 100, 50),
-    }
-    user_inputs = {}
-    for param, (min_val, max_val, default_val) in params.items():
-        user_inputs[param] = st.sidebar.slider(param, min_value=min_val, max_value=max_val, value=default_val)
-
     # Function to predict specific energy based on user inputs
     def predict_specific_energy(operational_params):
         input_data = pd.DataFrame(operational_params, index=[0])
